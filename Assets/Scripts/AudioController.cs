86b5519c10a3f9f4e7208b4998c1d43ecccea745
TJSteel <jaysteel78@msn.com>
2020-01-05 14:51:57 +0000

    added basic start scene
    player is added with required controllers
    audio controller has been added
    camera controller has been added
    setup scene with a box for the floor, and the player prefab
    (player has no sprite so enable gizmos to see this)

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour
+{
+	private static float musicvol;
+	private static float soundvol;
+
+	// Start is called before the first frame update
+	private void Awake()
+	{
+		GameObject[] multiAudio = GameObject.FindGameObjectsWithTag("audio");
+		if (multiAudio.Length > 1)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void Start()
+    {
+		musicvol = PlayerPrefs.GetFloat("MusicVolume", 1);
+		soundvol = PlayerPrefs.GetFloat("SoundVolume", 1);
+        ChangeVolume("Soundtrack", musicvol);
+		DontDestroyOnLoad(gameObject);
+    }
+
+	public void SettingsVolume()
+	{
+		musicvol = PlayerPrefs.GetFloat("MusicVolume", 1);
+		soundvol = PlayerPrefs.GetFloat("SoundVolume", 1);
+        ChangeVolume("Soundtrack", musicvol);
+    }
+
+	public static void PlaySound(string clipName) {
+        GameObject clip = GameObject.Find(clipName);
+        if (clip == null) {
+            Debug.Log("AudioController.PlaySound could not find a clip named: " + clipName);
+            return;
+        }
+        AudioSource audioSource = clip.GetComponent<AudioSource>();
+        if (audioSource) {
+            audioSource.volume = soundvol;
+
+            audioSource.Play();
+        }
+    }
+
+    public static void ChangeVolume(string clip, float volume) {
+        AudioSource audioSource = GameObject.Find(clip).GetComponent<AudioSource>();
+        if (audioSource == null) {
+            Debug.Log("AudioController.ChangeVolume could not find a clip named: " + clip);
+            return;
+        }
+        audioSource.volume = volume;
+        if (volume > 0 && !audioSource.isPlaying) {
+            audioSource.Play();
+        } else if (volume == 0 && audioSource.isPlaying) {
+            audioSource.Stop();
+        }
+    }
+
+    public static void IncrementPitch(string clip) {
+        AudioSource audioSource = GameObject.Find(clip).GetComponent<AudioSource>();
+        if (audioSource == null) {
+            Debug.Log("AudioController.IncrementPitch could not find a clip named: " + clip);
+            return;
+        }
+        audioSource.pitch += 0.1f;
+    }
+
+}
---
+++ Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour {
+
+    public float offsetUp;
+    public float offsetDown;
+    public float offsetLeft;
+    public float offsetRight;
+    public bool lockX;
+    public bool lockY;
+    
+    private GameObject cameraBounds;
+    private GameObject player;
+
+    private void Start() {
+        cameraBounds = GameObject.Find("CameraBounds");
+        player = GameObject.Find("Player");
+    }
+
+    void LateUpdate() {
+
+        float height = GetComponent<Camera>().orthographicSize;
+        // careful of the order below, Screen.width is int not float so will be rounded
+        float width = height * Screen.width / Screen.height;
+
+        float playerX = player.transform.position.x;
+        float x = transform.position.x;
+        float playerY = player.transform.position.y;
+        float y = transform.position.y;
+        float offsetUp = height * (this.offsetUp / 100);
+        float offsetDown = height * (this.offsetDown / 100);
+        float offsetLeft = width * (this.offsetLeft / 100);
+        float offsetRight = width * (this.offsetRight / 100);
+
+        /* the way the movement is handled, allows the camera to move only if you're passing the center of the screen, 
+         / or are past the offset, this way the camera will not lock onto you and makes it smoother overall
+         / 
+         / also check that the camera is not wider / taller than the bounds, if it is lock it to the center
+        */
+        if (cameraBounds != null && width * 2 > cameraBounds.transform.localScale.x) {
+            x = cameraBounds.transform.position.x;
+        } else if (!lockX) {
+            // move the camera if the player is passed the center, or left of the offset
+            if (playerX > (x + offsetRight)) {
+                x = playerX - offsetRight;
+            } else if (playerX < (x - offsetLeft)) {
+                x = playerX + offsetLeft;
+            }
+            if (cameraBounds != null) {
+                // check the bounds are not ouside of the game area
+                float leftBound = cameraBounds.transform.position.x - (cameraBounds.transform.localScale.x / 2);
+                float rightBound = cameraBounds.transform.position.x + (cameraBounds.transform.localScale.x / 2);
+                if (x - width < leftBound) {
+                    x = leftBound + width;
+                } else if (x + width > rightBound) {
+                    x = rightBound - width;
+                }
+            }
+        }
+        if (cameraBounds != null && height * 2 > cameraBounds.transform.localScale.y) {
+            y = cameraBounds.transform.position.y;
+        } else if (!lockY) {
+            // move the camera if the player is passed the center, or below the offset
+            if (playerY > (y + offsetUp)) {
+                y = playerY - offsetUp;
+            } else if (playerY < (y - offsetDown)) {
+                y = playerY + offsetDown;
+            }
+            if (cameraBounds != null) {
+                // check the bounds are not ouside of the game area
+                float lowerBound = cameraBounds.transform.position.y - (cameraBounds.transform.localScale.y / 2);
+                float upperBound = cameraBounds.transform.position.y + (cameraBounds.transform.localScale.y / 2);
+                if (y - height < lowerBound) {
+                    y = lowerBound + height;
+                } else if (y + height > upperBound) {
+                    y = upperBound - height;
+                }
+            }
+        }
+
+        transform.position = new Vector3(x, y, transform.position.z);
+
+    }
+}
